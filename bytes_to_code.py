6d6156bc8e922e30673c8b08f139f8b1eb553ece
yuvyuvyuv <yuvyuv500@gmail.com>
2024-07-08 15:58:34 +0300

    bytes_to_code datamatrix

--- bytes_to_code.py
+++ bytes_to_code.py
@@ -1,4 +1,15 @@
 import qrcode
+from pylibdmtx.pylibdmtx import encode
+from PIL import Image
+
+def generate_data_matrix(input_file, output_file):
+    with open(input_file, 'rb') as file:
+        binary_data = file.read()
+    encoded = encode(binary_data)
+    img = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
+    img.save(output_file)
+    return output_file
+
 
 def generate_qr_code(input_file, output_file):
     # Read binary data from file
@@ -26,6 +37,6 @@ def generate_qr_code(input_file, output_file):
 
 if __name__ == "__main__":
     input_file = "data_for_checks\check_hadar.txt"  # Replace with your binary file path
-    output_file = "qr_code.png"  # Output QR code file name
+    output_file = input_file[:-3]+"png"  # Output QR code file name
 
-    generate_qr_code(input_file, output_file)
+    generate_data_matrix(input_file, output_file)
