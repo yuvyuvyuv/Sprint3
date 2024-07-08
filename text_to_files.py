c95c9efe0ef9c190f5821b09775ceb855aa0b511
yuvyuvyuv <yuvyuv500@gmail.com>
2024-07-08 18:15:57 +0300

    text_tofiles

--- text_to_files.py
+++ text_to_files.py
@@ -1,15 +1,23 @@
-def text_to_file(text_data, output_file):
+def create_all_files(big_string):
     start_marker = "file_format:"
-    end_marker = "end"
-    
-    # Find the start and end positions of the file format
-    start_pos = text_data.find(start_marker) + len(start_marker)
-    end_pos = text_data.find(end_marker, start_pos)
-    
-    # if start_pos != -1 and end_pos != -1:
-    #     # Extract the file format
-    file_format = text_data[start_pos:end_pos]
-    
-    bytes_data = text_data[end_pos+1:].encode('latin1')
+    output_file = "out"
+    i = 0
+    files = big_string.split(start_marker)
+    for file_text in files:
+        i += 1
+        text_to_file(file_text,output_file+i)
 
-    
+
+def text_to_file(text_data,output_file):
+    end_marker = "end"
+    end_pos = text_data.find(end_marker)
+    file_format = text_data[:end_pos]
+    end_of_file = text_data.find("end_of_file")
+    bytes_data = text_data[end_pos+1:end_of_file].encode('latin1')
+    output_file_with_format = output_file+file_format
+    with open(output_file_with_format, 'wb') as file:
+        file.write(bytes_data)
+
+
+
+
