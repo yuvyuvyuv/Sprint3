f59ef0d41faf8bd34033d84f53346783572c6ae4
yuvyuvyuv <yuvyuv500@gmail.com>
2024-07-08 14:50:52 +0300

    bytes to qrcode

---
+++ bytes_to_code.py
@@ -0,0 +1,30 @@
+import qrcode
+
+def generate_qr_code(input_file, output_file):
+    # Read binary data from file
+    with open(input_file, 'rb') as file:
+        binary_data = file.read()
+
+    # Generate QR code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(binary_data)
+    qr.make(fit=True)
+
+    # Create QR code image
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+
+    # Save QR code image to file
+    qr_image.save(output_file)
+
+    print(f"QR code saved as {output_file}")
+
+if __name__ == "__main__":
+    input_file = "data_for_checks\check_hadar.txt"  # Replace with your binary file path
+    output_file = "qr_code.png"  # Output QR code file name
+
+    generate_qr_code(input_file, output_file)
