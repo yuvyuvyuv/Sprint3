cc9f9923ee500faf08883bd729eea1f9532b9b46
yuvyuvyuv <yuvyuv500@gmail.com>
2024-07-08 17:55:35 +0300

    text to fiels

--- text_to_files.py
+++ text_to_files.py
@@ -1,27 +1,15 @@
-def file_to_barcodes(file_path, file_number):
-    file_path = os.path.join(DIR_PATH, file_path)
-    file_format = os.path.splitext(file_path)[1]
-    with open(file_path, 'rb') as file:
-        text = file.read()
-        text = text.decode('latin1')
-        # split text into chunks of 100 characters
+def text_to_file(text_data, output_file):
+    start_marker = "file_format:"
+    end_marker = "end"
+    
+    # Find the start and end positions of the file format
+    start_pos = text_data.find(start_marker) + len(start_marker)
+    end_pos = text_data.find(end_marker, start_pos)
+    
+    # if start_pos != -1 and end_pos != -1:
+    #     # Extract the file format
+    file_format = text_data[start_pos:end_pos]
+    
+    bytes_data = text_data[end_pos+1:].encode('latin1')
 
-        chunks = []
-        chunk_size = 100
-        for i in range(0, len(text), chunk_size):
-            chunk = text[i:i + chunk_size]
-            chunks.append(chunk)
-
-        # generate QR codes for each chunk
-        qr_codes = []
-        start_file = "file_format:"+file_format+"end"
-        start_barcode = generate_qr_code_from_text(start_file, f"start_barcode{file_number}.png")
-        qr_codes.append(start_barcode)
-        for i, chunk in enumerate(chunks):
-            output_file = f"qr_code_{file_number}_{i}.png"
-            image_path = generate_qr_code_from_text(chunk, output_file)
-            # convert data type to png
-            qr_codes.append(image_path)
-        end_barcode = generate_qr_code_from_text("end_of_file", f"end_barcode{file_number}.png")
-        qr_codes.append(end_barcode)
-        return qr_codes
+    
